8049bc583da7d9dcd4db5498c3525f81c18a9f8b
BDmurad1610 <128228088+BDmurad1610@users.noreply.github.com>
2026-01-28 23:06:18 +0600

    Delete delete_all.sh

--- delete_all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# This script deletes all old files
-rm -f index.html login.html dashboard.html premium.html
-rm -f style.css script.js api.js
-rm -f README.md
-echo "All old files deleted!"
